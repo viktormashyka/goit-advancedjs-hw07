64ff0eee5f2c3d49196efbf10a8fcd7be5f6d4c0
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2023-12-05 22:28:22 +0000

    complete task

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,51 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean;
+  public tenants: Person[];
+
+  constructor(public key: Key) {
+    this.tenants = [];
+  }
+
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  public abstract openDoor(key: Key): boolean;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  public openDoor(key: Key): boolean {
+    return key.getSignature() === this.key.getSignature() && this.door === true;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -7,5 +55,6 @@ house.openDoor(person.getKey());
 
 house.comeIn(person);
 
+console.log(house.openDoor(person.getKey()));
 
 export {};
